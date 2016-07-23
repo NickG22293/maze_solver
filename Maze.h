c7ec82d08785847b110f5301818f2e4634a77cbd
Nick Gordon <ngordon22293@gmail.com>
2016-07-23 15:02:23 -0600

    Create Maze interface

--- Maze.h
+++ Maze.h
@@ -1,3 +1,24 @@
 /* A maze is comprised of many nodes, linked together with two nodes 
    representing beginning and end. Solver's will attempt to find the 
    end of the graph, possibly in a variety of algorithmic ways. */ 
+
+class Maze 
+{
+	typedef std::vector<std::vector> Grid; 
+public:
+	Maze(); 
+	Maze(int grid_size); 
+	//Maze(SolverPool pool); 	
+
+private:
+	SolverPool 	m_Pool; 
+	Grid 		m_Grid; 
+
+	// Kick off execution of solving the maze
+	void Solve(); 
+	// Run through and create connections between nodes
+	void ConnectMaze(); 
+	// Set the end and beginning nodes
+	void MakeNodeEnd(Node n); 
+	void MakeNodeBeginning(Node n);
+}
