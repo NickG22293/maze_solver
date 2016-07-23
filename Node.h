2675483cbe0b717b4eb6602d445a19a02e269d48
Nick Gordon <ngordon22293@gmail.com>
2016-07-23 15:06:23 -0600

    Flesh out Node interface

--- Node.h
+++ Node.h
@@ -5,19 +5,38 @@ struct Point
 	int y = -1; 
 }
 
+enum eNodeStatus
+{
+	START, 
+	NEUTRAL,
+	END
+}
+
 /* the maze is made up of series of Nodes in a grid 
    a Node is just coordinates, and links to connecting 
    nodes. One node will have starting condition, 
    and another will have ending condition */
 class Node 
 {
+	typedef NodeStatus int; 
 public: 
 	Node(); 
 	Node(int x, int y); 
+
+	//Retrieve coordinates in maze grid
 	Point GetCoords(); 
+	// End condition 
 	bool isEnd(); 
+	// No connecting nodes besides previous 
 	bool isDeadEnd(); 
 
 private:	
-	std::vector<const Point> m_ConnectingNodes; 	
+	// Connect given node to this 
+	void ConnectNode(std::unique_ptr<Node> n); 
+	void SetCoords(int x, int y); 
+
+	// Container of all connected nodes (max 4)
+	std::vector<const Point> m_ConnectingNodes;
+	Point m_Coords; 
+	NodeStatus m_Status; 	
 } 
